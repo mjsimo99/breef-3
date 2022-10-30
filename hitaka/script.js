aefab4912daa3267f048deb06870a911240ab82d
mjsimo99 <mohashark21@gmail.com>
2022-10-30 15:58:12 +0100

    button select

--- hitaka/script.js
+++ hitaka/script.js
@@ -3,60 +3,48 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active')
 })
 
 // for visibility card
-var shop = document.querySelector(".shop");
-var payment = document.querySelector(".payment");
-shop.addEventListener("click", function(){
-    payment.classList.toggle("shadow");
-})
+var shop = document.querySelectorAll(".shop");
+for (var i = 0; i < shop.length; i++) {
+    var firstt = shop[i];
+    firstt.addEventListener("click", function (e) {
+        var secendd = e.target;
+        var thirdd = secendd.parentElement.parentElement.parentElement.parentElement.parentElement.children[3].classList.toggle("shadow");
+        console.log(thirdd);
+
+    })
+}
 
 // for plus minus
 var btnBuyPlus = document.getElementsByClassName("buyplus");
-for (var i = 0; i < btnBuyPlus.length; i++){
+for (var i = 0; i < btnBuyPlus.length; i++) {
     var first = btnBuyPlus[i];
-    first.addEventListener("click", function(event) {
+    first.addEventListener("click", function (event) {
         var secend = event.target;
         var third = secend.parentElement.parentElement.parentElement.children[3].children;
         var fourth = third[0].value;
         var newvalue = parseInt(fourth) + 1;
         console.log(newvalue);
-        third[0].value = newvalue
+        third[0].value = newvalue;
     })
 }
 var btnBuyMinus = document.getElementsByClassName("buyminus");
-for (var i = 0; i < btnBuyMinus.length; i++){
+for (var i = 0; i < btnBuyMinus.length; i++) {
     var first = btnBuyMinus[i];
-    first.addEventListener("click", function(event) {
+    first.addEventListener("click", function (event) {
         var secend = event.target;
         var third = secend.parentElement.parentElement.parentElement.children[3].children;
         var fourth = third[0].value;
-        if (parseInt(fourth) > 1){
-        let newvalue = parseInt(fourth) - 1;
-        console.log(newvalue);
-        third[0].value = newvalue;
-    }
+        if (parseInt(fourth) > 1) {
+            let newvalue = parseInt(fourth) - 1;
+            console.log(newvalue);
+            third[0].value = newvalue;
+        }
 
     })
 }
 // __________________
-// var btnBuyPlus = document.querySelector(".buyplus");
-// var btnBuyMinus = document.querySelector(".buyminus");
-// var valueinput = parseInt(document.querySelector(".numbershop").value);
-// console.log(valueinput);
-// btnBuyPlus.addEventListener("click", function(){
-//     valueinput++;
-//     document.querySelector(".numbershop").value = valueinput;
-// })
-// btnBuyMinus.addEventListener("click", function(){
-//     if (valueinput > 1){
-//     valueinput--;
-// }
-//     document.querySelector(".numbershop").value = valueinput;
-// })
-
-
-
 
