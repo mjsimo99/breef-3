0fbfc5a79a1b449578934ffe608fbb107e1b548a
mjsimo99 <mohashark21@gmail.com>
2022-11-15 21:22:53 +0100

    fix card

--- hitaka/script.js
+++ hitaka/script.js
@@ -1,90 +1,158 @@
 // for menu toggle
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active');
 })
 
-// for visibility card
-var shop = document.querySelectorAll(".shop");
-for (var i = 0; i < shop.length; i++) {
-    var firstt = shop[i];
-    firstt.addEventListener("click", function (e) {
-        var secendd = e.target;
-        var thirdd = secendd.parentElement.parentElement.parentElement.parentElement.parentElement.children[3].classList.toggle("shadow");
-        console.log(thirdd);
 
-
-    })
-}
-
-// for plus minus
-var btnBuyPlus = document.getElementsByClassName("buyplus");
-for (var i = 0; i < btnBuyPlus.length; i++) {
-    var first = btnBuyPlus[i];
-    first.addEventListener("click", function (event) {
-        var secend = event.target;
-        var third = secend.parentElement.parentElement.parentElement.children[3].children;
-        var fourth = third[0].value;
-        var newvalue = parseInt(fourth) + 1;
-        console.log(newvalue);
-        third[0].value = newvalue;
-    })
-}
-var btnBuyMinus = document.getElementsByClassName("buyminus");
-for (var i = 0; i < btnBuyMinus.length; i++) {
-    var first = btnBuyMinus[i];
-    first.addEventListener("click", function (event) {
-        var secend = event.target;
-        var third = secend.parentElement.parentElement.parentElement.children[3].children;
-        var fourth = third[0].value;
-        if (parseInt(fourth) > 1) {
-            let newvalue = parseInt(fourth) - 1;
-            console.log(newvalue);
-            third[0].value = newvalue;
-        }
-
-    })
-}
 //if chose one hide other __________________
 document.querySelector(".A1").addEventListener("click", function () {
-    document.querySelector("body > div.mother > div.card1 ").style.display = "flex";
-    document.querySelector("body > div.mother > div.card2 ").style.display = "none";
-    document.querySelector("body > div.mother > div.card3 ").style.display = "none";
+    document.querySelector("body > div.grandM > div.mother > div.card1 ").style.display = "flex";
+    document.querySelector("body > div.grandM > div.mother > div.card2 ").style.display = "none";
+    document.querySelector("body > div.grandM > div.mother > div.card3 ").style.display = "none";
 })
 document.querySelector(".A2").addEventListener("click", function () {
-    document.querySelector("body > div.mother > div.card1 ").style.display = "none";
-    document.querySelector("body > div.mother > div.card2 ").style.display = "flex";
-    document.querySelector("body > div.mother > div.card3 ").style.display = "none";
+    document.querySelector("body > div.grandM > div.mother > div.card1 ").style.display = "none";
+    document.querySelector("body > div.grandM > div.mother > div.card2 ").style.display = "flex";
+    document.querySelector("body > div.grandM > div.mother > div.card3 ").style.display = "none";
 })
 document.querySelector(".A3").addEventListener("click", function () {
-    document.querySelector("body > div.mother > div.card1 ").style.display = "none";
-    document.querySelector("body > div.mother > div.card2 ").style.display = "none";
-    document.querySelector("body > div.mother > div.card3 ").style.display = "flex";
+    document.querySelector("body > div.grandM > div.mother > div.card1 ").style.display = "none";
+    document.querySelector("body > div.grandM > div.mother > div.card2 ").style.display = "none";
+    document.querySelector("body > div.grandM > div.mother > div.card3 ").style.display = "flex";
 })
 // for show all
 document.querySelector(".A4").addEventListener("click", function () {
-    document.querySelector("body > div.mother > div.card1 ").style.display = "flex";
-    document.querySelector("body > div.mother > div.card2 ").style.display = "flex";
-    document.querySelector("body > div.mother > div.card3 ").style.display = "flex";
+    document.querySelector("body > div.grandM > div.mother > div.card1 ").style.display = "flex";
+    document.querySelector("body > div.grandM > div.mother > div.card2 ").style.display = "flex";
+    document.querySelector("body > div.grandM > div.mother > div.card3 ").style.display = "flex";
 })
-// add cart
-// var addItemId = 0
-// function addToCart(item) {
-//     var selectedItem = document.createComment('div');
-//     selectedItem.classList.add('cartImg');
-//     console.log(selectedItem)
-//     selectedItem.setAttribute('id', addItemId);
-//     var img = document.createElement('img');
-//     img.setAttribute('src', item.parentElement.parentElement.parentElement.parentElement.children[0].children[0].currentSrc);
-//     console.log(amine);
+// for cards
 
-//     var cartItem = document.getElementById('title');
-//     selectedItem.append(img);
-//     cartItem.append(selectedItem);
+var total = 0;
+var i = 1;
+// Message 
+var message = "Please select a size";
+// List of the amount of every product in the cart
+var itemCost = [];
+//
+// Add to cart
+function add(n) {
+    sizeId = "size" + n;
+    console.log(sizeId);
+    messageId = "message" + n;
+    console.log(messageId);
+    // The size of the selected card
+    size = document.getElementById(sizeId).value;
+    console.log(size);
+    // message show
+    if (size === "size") {
+        document.getElementById(messageId).innerHTML = message;
+        return 0;
 
-// }
+    }
+    //select card name price quantity 
+    brand = "brand" + n;
+    priceId = "price" + n;
+    quantityId = "quantity" + n;
+    // brand name
+    name = document.getElementById(brand).innerHTML;
+    console.log(name);
+    // price
+    price = document.getElementById(priceId).innerHTML;
+    console.log(price);
+    // quantity
+    quantity = document.getElementById(quantityId).value;
+    console.log(quantity);
+
+    // Creat balise li  
+    var node = document.createElement("li");
+    // creat id li
+    item = "item" + i;
+    node.setAttribute("id", item);
+    // calcul price * quantity
+    itemCost[i - 1] = Number(price) * Number(quantity);
+    console.log(itemCost[i - 1]);
+
+    i += 1;
+    console.log(i);
+    // text for cart
+    var textnode = document.createTextNode(name + " " + quantity + " x " + price + " $, size: " + size);
+    // add the text to li
+    node.appendChild(textnode);
+    // add li element to ul list
+    document.getElementById("items").appendChild(node);
+
+    total += Number(price) * Number(quantity);
+    console.log(total);
+    // update the total
+    document.getElementById("total").innerHTML = "Total: " + total.toFixed(2) + " $";
+
+    // remove button 
+    document.getElementById(item).innerHTML += '<button class="buttonbuyitem" onclick="deleItem(' + "'" + item + "'" + ')">x</button>';
+}
+
+// delete message 
+function deleteE(eId) {
+    document.getElementById(eId).innerHTML = ' ';
+    console.log(document.getElementById(eId).innerHTML);
+}
+
+// Remove a product from the cart
+function deleItem(eId) {
+    document.getElementById(eId).remove();
+    n = Number(eId.slice(-1)) - 1;
+    console.log(n);
+    total -= itemCost[n];
+    console.log(total);
+    document.getElementById("total").innerHTML = "Total: " + total.toFixed(2) + " $";
+}
+
+
+
+
+
+
+
+
+        // ______________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
